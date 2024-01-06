4dd3e6b23df8ae9fa3ce9b72cb13e990a3814570
bhavilpatel <bhavilpatel976@gmail.com>
2024-01-06 18:28:24 +0530

    added footer

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import logo from '../Assets/Gemstone logo_pages-to-jpg-0001.jpg'
+import '../components/css/Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <Container>
+        <div className='row'>
+          <div className='col-md-4 mt-4 '>
+            <div className='logo'><img src={logo} height="100%" width="100%" /></div>
+            <hr />
+            <p className='text-center '>© copyright 2024. All rights reserved.</p>
+          </div>
+          <div className='col-md-4 '></div>
+          <div className='col-md-4 d-flex justify-content-center mt-4 '>
+            <div style={{width:"object-fit-contain "}}>
+            <div>Crystall Mall,<br />Kalavad road, <br /> Rajkot - 360005</div>
+            <div className='mt-4 '>+91 9724528889</div>
+            <div className='mt-4 '>Gemstone@gamil.com</div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
--- src/Layout/Headder.js
+++ src/Layout/Headder.js
@@ -2,49 +2,48 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../components/css/Headder.css'
+import logo from '../Assets/Gemstone logo_pages-to-jpg-0001.jpg'
 
 function Headder() {
   return (
     <Navbar collapseOnSelect expand="lg" className="nav">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/home" className='logo'><img src={logo} alt="Logo" height="100%" width="100%" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <div class="dropdown">
-              <button class="dropbtn">MENS</button>
-              <div class="dropdown-content">
+            <div className="dropdown">
+              <Nav.Link className="dropbtn">MENS</Nav.Link>
+              <div className="dropdown-content">
                 <a href="#">Rings</a>
                 <a href="#">Bracelets</a>
                 <a href="#">Chains</a>
                 <a href="#">Leather Bracelets</a>
               </div>
             </div>
-            <div class="dropdown">
-              <button class="dropbtn">WOMENS</button>
-              <div class="dropdown-content">
+            <div className="dropdown">
+              <Nav.Link className="dropbtn">WOMENS</Nav.Link>
+              <div className="dropdown-content">
                 <a href="#">Rings</a>
                 <a href="#">Bracelets</a>
                 <a href="#">Chains</a>
                 <a href="#">Necklace</a>
                 <a href="#">Earrings</a>
                 <a href="#">Bangles</a>
-                <a href="#"></a>
               </div>
             </div>
-            <div class="dropdown">
-              <button class="dropbtn">BEST SELLERS</button>
-              <div class="dropdown-content">
+            <div className="dropdown">
+              <Nav.Link className="dropbtn">BEST SELLERS</Nav.Link>
+              <div className="dropdown-content">
                 <a href="#">Rings</a>
                 <a href="#">Bracelets</a>
                 <a href="#">Chains</a>
                 <a href="#">Necklace</a>
                 <a href="#">Earrings</a>
                 <a href="#">Bangles</a>
-                <a href="#"></a>
               </div>
             </div>
-            <Nav.Link href="#home" className='dropbtn text-light '>CONTACT US</Nav.Link>
+            <Nav.Link href="#home" className='dropbtn '>CONTACT US</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
